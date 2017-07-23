813caa6efef67c1931072dd88200e042fd0bf875
Jonathan Mancia <jonathanman2010@icloud.com>
2017-07-22 23:52:48 -0600

    yarn.lock file added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.use('/public', express.static('public'))
 
@@ -7,4 +8,11 @@ app.get('/', (req, res) => {
   res.render('index.pug', {title: 'Jonathan'});
 });
 
+fs.readFile('my_data.csv','utf8', function(err, data){
+  json(data);
+  data.forEach( function(hi){
+    console.log(hi)
+  })
+});
+  
 app.listen(3000);
--- javascript/App.js
+++ javascript/App.js
@@ -6,9 +6,11 @@ import 'bootstrap-css';
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = { names: ['Jonathan', 'Lacie'], url: 'https://cdn.pixabay.com/photo/2017/04/02/14/46/philippines-2196004_960_720.jpg'}
-  }
+  
+  const d = new Date();
+  const year = () => d.getFullYear();
 
+  }
   render(){
     return(
       <div>
@@ -16,7 +18,8 @@ class App extends React.Component {
           <div className="container-fluid">
             <div className="jumbotron">
               <div className="container">
-                <h1 className="text-center">Hi there {this.state.names[1]}!</h1>
+                <h1 className="text-center">Welcome</h1>
+                <div className="text-center"><p>{this.year}</p></div>
               </div>
             </div>
           </div>
